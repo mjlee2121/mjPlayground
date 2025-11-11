ff70d76748c5647300a571bc7df356422631e455
minji <lee.minji9521@gmail.com>
2025-11-11 13:28:13 -0500

    added target=blank and some css work on portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useEffect } from 'react'
 import NavBar from '../component/NavBar'
 import './Portfolio.css'
+import tictactoeImage from '../assets/portfolio/tictac.png'
 
 const Portfolio = () => {
   useEffect(()=>{
@@ -13,15 +14,26 @@ const Portfolio = () => {
     },[])
 
   return (
-    <div className='body'>
+    <div className='portfolio'>
       <NavBar />
       <div className='tic-tac-toe'>
-        <h1>d</h1>
-        <h1>d</h1>
-        <h3>Tic Tac Toe App</h3>
-          <p>You can play tic tac toe on this app. It will show who's the next player and who's the winner</p>
+        <h1>Tic Tac Toe App</h1>
+        <div className='tic-tac-toe-content'>
+          <div className='tic-tac-toe-text'>
+            <p>You can play tic tac toe on this app. It will show who's the next player and who's the winner</p>
             <p>When the game is over, it will allow you to go back to a specific move</p>
-            <a href="https://mjlee2121.github.io/react-tictactoe-app/"><i class="fa-solid fa-link"></i></a>
+            <a href="https://mjlee2121.github.io/react-tictactoe-app/" target='_blank'>Click HERE</a>
+          </div>
+          <div className='tic-tac-toe-image'>
+            <img src={tictactoeImage} alt="tic-tac-toe-img" className="tic-tac-toe-img" />
+          </div>
+          
+        </div>
+        
+
+          
+          
+          
 
       </div>
       
